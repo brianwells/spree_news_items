745e4bdb907582e894e1b99fe2d0bb8a9b1fdeab
Brian Wells <spam_brian@me.com>
2013-12-27 23:34:42 -0500

    Complete Spree::NewsItem model

--- app/models/spree/news_item.rb
+++ app/models/spree/news_item.rb
@@ -1,2 +1,25 @@
-class Spree::NewsItem < ActiveRecord::Base
+module Spree
+  class NewsItem < ActiveRecord::Base
+  
+    validates_presence_of :title, :category
+
+    scope :enabled, lambda { |*categories|
+      if categories.empty?
+        where(:enabled => true)
+      else
+        where(:enabled => true).where(:category => categories)
+      end
+    }
+
+    def initialize(*args)
+      super(*args)
+      last_item = NewsItem.last
+      self.position = last_item ? last_item.position + 1 : 0
+    end
+
+    def self.categories_for_select
+      unscoped.pluck(:category).uniq.sort
+    end
+
+  end
 end
