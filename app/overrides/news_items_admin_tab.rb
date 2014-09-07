cb3411b794ebba50baca7d93c3468a9f52906fde
Brian Wells <spam_brian@me.com>
2014-09-06 22:40:58 -0400

    Update font icon names

--- app/overrides/news_items_admin_tab.rb
+++ app/overrides/news_items_admin_tab.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                       :name => "news_items_admin_tab",
                       :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                      :text => "<%= tab(:news_items, :label => Spree.t(:news_items), :icon => 'icon-rss') %>")
+                      :text => "<%= tab(:news_items, :label => Spree.t(:news_items), :icon => 'rss') %>")
